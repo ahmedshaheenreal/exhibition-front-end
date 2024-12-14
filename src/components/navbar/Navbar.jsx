f63d46b91128cd52d52f4859cc36966c7c088340
Loai Mekbel <loai@192.168.1.69>
2024-12-14 20:40:51 +0300

    navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Exhibition from "../../pages/Exhibition";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div>
+        <a href="/">Home</a>
+        <a href="/">Exhibition</a>
+        <a href="/">Artists </a>
+        <a href="/">About us</a>
+      </div>
+      <div>
+        <a href="/">Search</a>
+        <a href="/">login</a>
+      </div>
+    </nav>
+  );
+};
