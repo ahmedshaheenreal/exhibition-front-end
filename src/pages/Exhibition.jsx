a7c3d144bf87ef2624cb8a687d96c38f267217c6
Ahmed shaheen <ahmedshaheenwork@gmail.com>
2024-12-14 16:58:22 +0200

    fixing bugs with the setup

---
+++ src/pages/Exhibition.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./Exhibition.module.css";
+function Exhibition() {
+  return (
+    <main>
+      <div className={styles.exhibitionContainer + " container"}></div>
+    </main>
+  );
+}
+
+export default Exhibition;
