a8e2f25bca9a77ebe9d0dda3d3a97378f85e3d25
Loai Mekbel <loai@192.168.1.73>
2024-12-15 18:18:31 +0300

    navbar done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,19 +1,34 @@
-import React from "react";
-import Exhibition from "../../pages/Exhibition";
-
-const Navbar = () => {
+import React, { useState } from "react";
+import styles from "./Navbar.module.css";
+import { NavLink } from "react-router-dom";
+export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
   return (
-    <nav className="navbar">
-      <div>
-        <a href="/">Home</a>
-        <a href="/">Exhibition</a>
-        <a href="/">Artists </a>
-        <a href="/">About us</a>
+    <div className={`${styles.mainContainer} `}>
+      <div className={`${styles.navbarContainer} container`}>
+        <div className={styles.navHeaderMobile}>
+          <NavLink>Logo</NavLink>
+          <div className={styles.menu} onClick={toggleMenu}>
+            {!menuOpen ? "Menu" : "close"}
+          </div>
+        </div>
+        <div className={`${styles.navbarLinks} ${menuOpen ? styles.open : ""}`}>
+          <nav className={`${styles.navbar} `}>
+            <div className={`${styles.group} ${styles.groupLeft}`}>
+              <NavLink href="/">Exhibition</NavLink>
+              <NavLink href="/">Artists </NavLink>
+              <NavLink href="/">About us</NavLink>
+            </div>
+            <div className={`${styles.group} ${styles.groupRight}`}>
+              <NavLink href="/">Search</NavLink>
+              <NavLink href="/">login</NavLink>
+            </div>
+          </nav>
+        </div>
       </div>
-      <div>
-        <a href="/">Search</a>
-        <a href="/">login</a>
-      </div>
-    </nav>
+    </div>
   );
 };
